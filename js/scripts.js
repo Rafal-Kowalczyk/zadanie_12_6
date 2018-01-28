c0baaa0f29e2176888da48a88895b5b7dd390992
Rafal-Kowalczyk <34551849+Rafal-Kowalczyk@users.noreply.github.com>
2018-01-28 13:02:46 +0100

    Add $(function(){});

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,25 @@
-var url = 'https://restcountries.eu/rest/v1/name/';
-var countriesList = $('#countries');
 
-$('#search').click(searchCountries);
+$(function(){
+	var url = 'https://restcountries.eu/rest/v1/name/';
+	var countriesList = $('#countries');
 
-function searchCountries() {
-  	var countryName = $('#country-name').val();
-	if(!countryName.length) countryName = 'Poland';
-	$.ajax({
-	  		url: url + countryName,
-	  		method: 'GET',
-	  		success: showCountriesList
-  	});
-}
+	$('#search').click(searchCountries);
 
-function showCountriesList(resp) {
-  	countriesList.empty();
-	resp.forEach(function(item){
-   	$('<li>').text('country: ' + item.name + ', stolica: ' + item.capital).appendTo(countriesList);
-   	//$('<li>').text('stolica: ' + item.capital).appendTo(countriesList);
-	});
-}
+	function searchCountries() {
+		var countryName = $('#country-name').val();
+		if(!countryName.length) countryName = 'Poland';
+		$.ajax({
+				url: url + countryName,
+				method: 'GET',
+				success: showCountriesList
+		});
+	}
+
+	function showCountriesList(resp) {
+		countriesList.empty();
+		resp.forEach(function(item){
+		$('<li>').text('country: ' + item.name + ', stolica: ' + item.capital).appendTo(countriesList);
+		//$('<li>').text('stolica: ' + item.capital).appendTo(countriesList);
+		});
+	}
+});	
